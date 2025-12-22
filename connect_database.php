548ca59c991c1b4ff1b672b1ed5ec2a57fdebc4d
Minh <minhhq.vnit@gmail.com>
2025-12-22 14:47:20 +0700

    Update database connection to include port configuration and remove hardcoded password

--- connect_database.php
+++ connect_database.php
@@ -1,10 +1,11 @@
 <?php
 $host = 'localhost';
 $user = 'root';
-$password = '@Root123456789';
+$password = '';
 $database = 'demo_basic';
+$port = 3333;
 
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($host, $user, $password, $database, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
